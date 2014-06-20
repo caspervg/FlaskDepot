9a9ac61655ca47d90f1cdf62c168b1d3421df93d
Casper Van Gheluwe <caspervg@gmail.com>
2014-06-20 10:42:57 +0200

    Populate correct sample data

--- flaskdepot/utils/populate.py
+++ flaskdepot/utils/populate.py
@@ -1,4 +1,5 @@
 import datetime
+from slugify import slugify
 from flaskdepot.extensions import db
 from flaskdepot.file.models import BroadCategory, NarrowCategory, File, Comment, Download, Vote
 from flaskdepot.user.models import Usergroup, User
@@ -38,12 +39,12 @@ def create_normal_user(username, password, email):
 
 
 def create_sample_data():
-    broadcat = BroadCategory(name=u'TestBroadCategory')
+    broadcat = BroadCategory(name=u'Sample BroadCategory')
     db.session.add(broadcat)
 
     db.session.commit()
 
-    narrwcat = NarrowCategory(name=u'TestNarrowCategory')
+    narrwcat = NarrowCategory(name=u'Sample NarrowCategory')
     broadcat.narrow_categories = [narrwcat]
     db.session.add(narrwcat)
 
@@ -51,22 +52,23 @@ def create_sample_data():
 
     user_1 = User.query.join(Usergroup).filter(Usergroup.is_admin).first()
 
-    file = File(name=u'TestFile', description=u'A test file', version=u'1.0', author=user_1,
-                num_views=3, file_name='MyTestFile.zip', preview1_name='MyPrev.png', preview2_name='MyPrev.gif',
-                broad_category=broadcat, narrow_category=narrwcat)
-    db.session.add(file)
+    _file = File(name=u'Sample file', description=u'This is a sample file. You can remove it.', version=u'1.0',
+                 author=user_1, num_views=3, file_name='sample-file.zip', preview1_name='sample-preview1.jpg',
+                 preview2_name='sample-preview2.jpg', broad_category=broadcat, narrow_category=narrwcat,
+                 slug=slugify('Sample file'))
+    db.session.add(_file)
 
     db.session.commit()
 
-    comment = Comment(text=u'Some nice comment text. We love you!', file=file, user=user_1)
+    comment = Comment(text=u'This is a sample comment. You can remove it.', file=_file, user=user_1)
     db.session.add(comment)
 
-    dl_1 = Download(last_downloaded=datetime.datetime.utcnow(), file=file, user=user_1)
-    dl_2 = Download(last_downloaded=datetime.datetime.utcnow(), file=file, user=user_1)
+    dl_1 = Download(last_downloaded=datetime.datetime.utcnow(), file=_file, user=user_1)
+    dl_2 = Download(last_downloaded=datetime.datetime.utcnow(), file=_file, user=user_1)
     db.session.add(dl_1)
     db.session.add(dl_2)
 
-    vote_1 = Vote(value=7, file=file, user=user_1)
+    vote_1 = Vote(value=4, file=_file, user=user_1)
     db.session.add(vote_1)
 
     db.session.commit()
